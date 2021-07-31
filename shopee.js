6186c632cdb90980d8ca7234c39331a7775f9e46
KongMike <monhandsome2006@gmail.com>
2021-07-31 16:16:03 +0700

    shopee

---
+++ shopee.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      img: "./images/new-product.jpg",
+      name: "Bread",
+      priceFrom: 200000,
+      priceTo: 409000,
+      sold: "sold 26,3K",
+    },
+    {
+      img: "./images/new-product.jpg",
+      name: "Mỳ ăn liền ô ma chi",
+      priceFrom: 5000,
+      priceTo: 9900,
+      sold: "sold 10,5K",
+    },
+  ];
+
+// Thao tác với id
+document.getElementById("ten_id")
+$('#ten_id')
+document.getElementsByClassName('ten_class')
+$('.ten_class')
+document.getElementsByTagName('ten_the');
+$('ten_the')
